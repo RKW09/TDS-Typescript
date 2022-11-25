f3daa8484cef2d37445a0738ba747d11b1925202
Douglas_Rodrigo <110440068+RKW09@users.noreply.github.com>
2022-11-24 23:07:43 -0300

    Update MensagemOculta.ts

--- MensagemOculta.ts
+++ MensagemOculta.ts
@@ -10,7 +10,7 @@ function inserirTexto()
 
     if(entrada == null || entrada == "" || regExp.test(entrada) || entrada.length > 50)
     {
-        feedback = "ERRO! A mensagem somente pode conter letras de a-z e ter até 50 caracteres.\n\n"
+        feedback = "ERRO! A mensagem somente pode conter espaços, letras de a-z e ter até 50 caracteres.\n\n"
         inserirTexto();
     }
     else
@@ -40,5 +40,5 @@ function decifrarMensagem(texto: string)
 inserirTexto();
 
 /*Link do Playground:
-https://www.typescriptlang.org/play?#code/MYewdgziA2CmB0w4EMBOAKAlAbgFC7gBcACAM1lgBMAjZYAa2IF5iAiVvXUgVzGEICW4YgMixUA1ABVYAD0IgsuAN65i64kWKwwhVMkrJmxAA6oQAWxOF05KrQbEA1GwCSkSUZDEZ8kAC5WHHwNTVgSVFgAcwBRWRN-YgAlaLiTYwB6AG0APWQAWgAvAB0IAF0MzlCBUnQdPQMjJhYwbmhoYgAfTu1dfUNmFnYunsjY+PhCWAgbev7kTBHehsN4ODAowgALYgA+YgBWAAZMNQ1VUNC7GjpGIZikpIB5AEJiAEFiCx0IZCjYCzEKDfXSwUwgShg0Cg1BhBoQYiQ4gFQraYhTWHIQgAS8OR2IwDQdAx03gxTA5NYZ0uIjEEmkcgUWDwoQAvtTtNAILAORcaYjYMAavpUABZH5-AF1PqNYJs3Dsri8fhCMACoWkEXiyCSizoKZ+RIzCQbU589RaX4CQxGvSiKLGdhVDQGhTGV0gSYSPXBDlWgZOFgerJHMrO9SkEAYLQCRKtCzUcTGI7YETEAA86MZnvWmy2qYETicp1C5suNX12ayAjKgzY9YAZA2s35q04AIy1l5DNgl-ll-lA5DWoyBlsKNudln81nEDns0LQqBwNYgKLof0LPCK0Tc+m+Jk4IA
+https://www.typescriptlang.org/play?#code/MYewdgziA2CmB0w4EMBOAKAlAbgFC7gBcACAM1lgBMAjZYAa2IF5iAiVvXUgVzGEICW4YgMixUA1ABVYAD0IgsuAN65i64kWKwwhVMkrJmxAA6oQAWxOF05KrQbEA1GwCSkSUZDEZ8kAC5WHHwNTVgSVFgAcwBRWRN-YgAlaLiTYwB6AG0APWQAWgAvAB0IAF0MzlCBUnQdPQMjJhYwbmhoYgAfTu1dfUNmFnYunsjY+PhCWAgbev7kTBHehsN4ODAowgALYgA+YgBWAAZMNQ1VUNC7GjpGIZikpIB5AEJiAEFiCx0IZCjYCzEKDfXSwUwgShg0Cg1DaCAmZAAc5AEAANGEGhBiJDiAVCtpiFNYchCABLw5HYjANB0InTeDFMCM1hnS4iMQSaRyBRYPChAC+rO00AgsCFFzZ2NgwBq+lQAFkfn8AXU+o1ggLcIKuLx+EIwFKZaQ5YrIMqLOgpn5EjMJBtThL1FpfgJDDa9KIosZ2FUNFaFMZ-SBJhILcEhS6Bk4WEGskcyr71KQQBgtAJEq0LNRxMYjtgRMQADyE7nB9abLb5gROJynUKOy41S2lrICMqDNidgBkXZLflbTgAjO2XkM2HXJQ3JUDkK6jNG+woB8O+ZL+cQhYLQtCoHA1iAouhIws8NrRKLOb4eTggA
 */
