11068c4ecacf0f083be26f1eacd374e53ec3b00f
Douglas_Rodrigo <110440068+RKW09@users.noreply.github.com>
2022-11-24 22:46:06 -0300

    Update MensagemOculta.ts

--- MensagemOculta.ts
+++ MensagemOculta.ts
@@ -1,13 +1,16 @@
 console.clear();
 
+let feedback = "";
+
 function inserirTexto()
 {
-    let entrada = prompt("Insira o Texto:");
+    let entrada = prompt(feedback + "Insira o Texto:");
 
     let regExp: RegExp = /[^a-z\s]/;
 
     if(entrada == null || entrada == "" || regExp.test(entrada) || entrada.length > 50)
     {
+        feedback = "ERRO! A mensagem somente pode conter letras de a-z e ter até 50 caracteres.\n\n"
         inserirTexto();
     }
     else
@@ -37,5 +40,5 @@ function decifrarMensagem(texto: string)
 inserirTexto();
 
 /*Link do Playground:
-https://www.typescriptlang.org/play?#code/MYewdgziA2CmB0w4EMBOAKAlAbgLACgCAzAVzGABcBLcAAisllStQBVYAPCkLAgbwK0htOBVqwwFVMgAmyWgF5aAB1QgAtsoroARAElILeSFrsuIAFw6cBQcNG1UsAOYBRDsou0ASi-fLFWgB6AG0APWQAWgAvAB0IAF0gvEJ8YXoidAkpWXkFJTASaGhaAB9S8UlpOUUlHR0yiqc3D3gKWAhtbOrkTEbKnLl4ODBnCgALWgA+WgBWAAZMOyEBNPThBggmFjNuLBT0gF9l8Wgtk9X14RlYYCoiaVQAWQkIZGdYdSyq3Js1oWO+EBxDIlBoYFoNzuDzQL0g70+6Ha5i8nWYoyW+Eu9lgYjeVDkqKkDGcgXqKROyO4gSpIDazC+fxO+JqAGolLSQvMEhT-rQiCAMA4qF5CuoAEZMQLzbD0WgAHlotOGEjG41lVFZrMx6WxV3uSM43BCVAStVoDQaADIrUqjSATayAIxmgCEdQtOqutD13qELPk7Lt5kdLoO3sOtBOgPSoEgMAQ0BAznQAb+wPwm22bHt+yAA
+https://www.typescriptlang.org/play?#code/MYewdgziA2CmB0w4EMBOAKAlAbgFC7gBcACAM1lgBMAjZYAa2IF5iAiVvXUgVzGEICW4YgMixUA1ABVYAD0IgsuAN65i64kWKwwhVMkrJmxAA6oQAWxOF05KrQbEA1GwCSkSUZDEZ8kAC5WHHwNTVgSVFgAcwBRWRN-YgAlaLiTYwB6AG0APWQAWgAvAB0IAF0MzlCBUnQdPQMjJhYwbmhoYgAfTu1dfUNmFnYunsjY+PhCWAgbev7kTBHehsN4ODAowgALYgA+YgBWAAZMNQ1VUNC7GjpGIZikpIB5AEJiAEFiCx0IZCjYCzEKDfXSwUwgShg0Cg1BhBoQYiQ4gFQraYhTWHIQgAS8OR2IwDQdAx03gxTA5NYZ0uIjEEmkcgUWDwoQAvtTtNAILAORcaYjYMAavpUABZH5-AF1PqNYJs3Dsri8fhCMACoWkEXiyCSizoKZ+RIzCQbU589RaX4CQxGvSiKLGdhVDQGhTGV0gSYSPXBDlWgZOFgerJHMrO9SkEAYLQCRKtCzUcTGI7YETEAA86MZnvWmy2qYETicp1C5suNX12ayAjKgzY9YAZA2s35q04AIy1l5DNgl-ll-lA5DWoyBlsKNudln81nEDns0LQqBwNYgKLof0LPCK0Tc+m+Jk4IA
 */
